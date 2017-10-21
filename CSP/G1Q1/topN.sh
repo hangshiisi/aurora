0c8f90e60d53ab90ae96e6d77309e09ae5b5d282
Hang Shi <hangshiisi@gmail.com>
2017-10-21 16:33:47 -0700

    G1 Q2

--- CSP/G1Q1/topN.sh
+++ CSP/G1Q1/topN.sh
@@ -6,9 +6,9 @@ hadoop fs -rm -r /output_T1/G1Q1
 
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.8.1.jar \
     -mapper mapper.py \
-    -reducer reducer.py \
+    -reducer topN.py \
     -input  /dataset_T1/G1Q1 \
-    -output /output_T1/G1Q1 \
+    -output /output_T1/topN \
     -file mapper.py \
-    -file reducer.py
+    -file topN.py
 
