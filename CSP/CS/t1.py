f3db0e39f7391062da221ea0003e2a7bc8b33b88
Hang Shi <hangshiisi@gmail.com>
2017-10-29 00:05:19 -0700

    cassandra python stuff

---
+++ CSP/CS/t1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python 
+
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType
+from pyspark import SQLContext
+
+
+#cluster = Cluster()
+#session = cluster.connect() 
+
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+spark.read\
+    .format("org.apache.spark.sql.cassandra")\
+    .options(table="kv", keyspace="test")\
+    .load().show()
+
+# write something 
+#test_data = spark.read.csv("f.csv", header=False) 
+test_data = spark.sparkContext.textFile("f.csv").map(\
+                    lambda l: l.strip().split(",")) 
+test_df = test_data.toDF(['k','v']) 
+
+test_df.show() 
+
+#insert into cassandra 
+test_df.write\
+    .format("org.apache.spark.sql.cassandra")\
+    .mode('append')\
+    .options(table="kv", keyspace="test")\
+    .save()
+
+#verify the results 
+spark.read\
+    .format("org.apache.spark.sql.cassandra")\
+    .options(table="kv", keyspace="test")\
+    .load().show()
