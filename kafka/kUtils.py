917622d33d2381631d0c87544a1d27bcff29d6fc
Hang Shi <hangshiisi@gmail.com>
2017-11-08 16:19:07 -0800

    kafka stuff

---
+++ kafka/kUtils.py
@@ -0,0 +1,24 @@
+import sys
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from uuid import uuid1
+if __name__ == “__main__”:
+    sc = SparkContext(appName=”PythonStreamingRecieverKafkaWordCount”)
+    ssc = StreamingContext(sc, 2) # 2 second window
+    broker, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, \
+                                  broker, \
+                                  “raw-event-streaming-consumer”,\
+                                  {topic:1}
+                                  ) 
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(“ “)) 
+                  .map(lambda word: (word, 1)) \
+                  .reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
+
+# spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 \
+#     kUtils.py localhost:9092 test
