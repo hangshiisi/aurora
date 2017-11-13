847ded19c26e5a7894b215b41e0d64bc6d2ea4f8
Hang Shi <hangshiisi@gmail.com>
2017-11-12 22:30:15 -0800

    flight cleanup

---
+++ PSC/G3/local_dataset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# data dir this folder needs to be available
+dataset_dir=/data/T3
+
+# dataset source dir
+src_dir=/data/aviation/airline_ontime/2008
+
+if [ -d $dataset_dir ]; then
+   echo "Folder $dataset_dir exists, script continue ..."
+   rm -rf $dataset_dir/*
+else
+   echo "Folder $dataset_dir does not exist, create the folder"
+   mkdir $dataset_dir
+fi
+
+for my_file in ${src_dir}/*
+do
+    fname=$(basename "$my_file")
+    base="${fname%.*}"    
+    echo "${base} and ${my_file}"  
+    unzip -p $my_file ${base}.csv | ./data_gen.py > ${dataset_dir}/${base}.txt
+done 
+
+echo 'Number of files cleaned:' 
+
+
